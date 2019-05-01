6ffe0d74f8a6e60b995e35a184ed1f42f5de2e4a
Mark Silvey <39961687+silveyy@users.noreply.github.com>
2019-05-01 15:32:14 -0700

    Automated commit

---
+++ OS Start Time.ps1
@@ -0,0 +1,16 @@
+﻿
+# Find last time system was turned on
+$os = Get-WmiObject -Class win32_operatingsystem
+$startTime = $os.ConvertToDateTime($os.LastBootUpTime)
+
+# Find out when first GP cycle after last boot completed
+
+#Get-EventLog -LogName System  -After $bootupTime | Where 
+
+
+$booted = (Get-WinEvent -FilterHashTable @{ LogName = "System"; StartTime = $startTime; ID = 6005 }).TimeCreated
+
+
+
+return $booted-$startTime
+#Get-WinEvent -LogName 'Microsoft-Windows-Winlogon/Operational' | select 
